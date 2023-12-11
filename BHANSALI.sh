11550d59be19de332f09ed2d05437977562ccfda
Mukesh Kumar Bhansali <bhansali.mukesh@gmail.com>
2023-12-11 11:36:23 +0530

    Update BHANSALI.sh

--- BHANSALI.sh
+++ BHANSALI.sh
@@ -2,23 +2,17 @@
 
 # Author : BHANSALI MUKESH KUMAR
 
-# http://shukriya-janaab.blogspot.com/
-# https://github.com/bhansali-mukesh/
-
 Kumar=${(%):-%N}
 export BHANSALI=`dirname ${Kumar}`
 
 # Open Myself
-alias BHANSALIam="vi $BHANSALI/BHANSALI.sh"
-
-# Enable Content Generator Tool
-. $BHANSALI/Content_Generator/GENERATOR.sh
+alias Bhansaliam="vi $BHANSALI/Bhansali.sh"
 
 # Enable General Aliases
-. $BHANSALI/General/PROFILE.sh
+#. $BHANSALI/General/PROFILE.sh
 
 # Enable Git Aliases
-. $BHANSALI/Git/Scripts/GIT.sh
+#. $BHANSALI/Git/Scripts/GIT.sh
 
 # Enable Shared Utilities
 . $BHANSALI/Shared/SEVAK.sh
@@ -26,16 +20,16 @@ alias BHANSALIam="vi $BHANSALI/BHANSALI.sh"
 # Enable Kubernetes Productivity
 . $BHANSALI/Kubernetes/KUBER.sh
 
-# Utility Function which Parses this File and Generates The BHANSALI Documents to Help,
-# The Command "BHANSALI" Generating Help Document from the Source on Runtime
-alias BHANSALI_Document='function MB() { Comment_Character="#"; Alias_Character="alias"; Help=""; Alias=""; while read line; do if [[ $line == $Comment_Character* ]]; then Help="$Help""\n""$line"; else Alias_Found=`echo "$line"| grep $Alias_Character`; if [[ M"$Alias_Found" != "M" ]]; then Alias=`echo "$line"| cut -d"=" -f1 | tr -s " "| cut -d" " -f2`; if [[ M"$Alias" != "M" ]]; then echo -e "\n$Alias" : "$Help"; Help=""; fi; else Help=""; fi; fi; done < "$Kumar"; }; MB'
+# Utility Function which Parses this File and Generates The Bhansali Documents to Help,
+# The Command "Bhansali" Generating Help Document from the Source on Runtime
+alias Bhansali_Document='function MB() { Comment_Character="#"; Alias_Character="alias"; Help=""; Alias=""; while read line; do if [[ $line == $Comment_Character* ]]; then Help="$Help""\n""$line"; else Alias_Found=`echo "$line"| grep $Alias_Character`; if [[ M"$Alias_Found" != "M" ]]; then Alias=`echo "$line"| cut -d"=" -f1 | tr -s " "| cut -d" " -f2`; if [[ M"$Alias" != "M" ]]; then echo -e "\n$Alias" : "$Help"; Help=""; fi; else Help=""; fi; fi; done < "$Kumar"; }; MB'
 
 
-# Just type "BHANSALI" on Terminal
-# If BHANSALI is Followed By Some Special Command then it will Show Help for that Command
+# Just type "Bhansali" on Terminal
+# If Bhansali is Followed By Some Special Command then it will Show Help for that Command
 # Else Displaying Help with Pagination ( Press Enter or Space to Scroll, "q" to Quite )
-# If you Add/Update Command(s) in this Utility with Proper Comment, it will Automatically Be Visible in BHANSALI (Help) as well.
+# If you Add, Update and Command in this Utility with Proper Comment, it will Automatically Be Visible in Bhansali as well.
         # Example
-                # 1. BHANSALI
-                # 2. BHANSALI Find
-alias BHANSALI='function JASOL() { Document=`BHANSALI_Document; Kuber_Document; Sevak_Document`; if [ M"$1" = "M" ]; then echo "$Document"| less; return; fi;  echo ""; echo "$Document" | sed -n "/^"$1"/,/^$/p"; }; JASOL'
+                # 1. Bhansali
+                # 2. Bhansali Find
+alias Bhansali='function JASOL() { Document=`Bhansali_Document; Kuber_Document; Sevak_Document`; if [ M"$1" = "M" ]; then echo "$Document"| less; return; fi;  echo ""; echo "$Document" | sed -n "/^"$1"/,/^$/p"; }; JASOL'
